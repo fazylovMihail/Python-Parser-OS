afaebf1f89d7a3a2495cf3d25e2da6f27d062387
Fazylov Mihail <161601509+fazylovMihail@users.noreply.github.com>
2025-01-27 22:13:03 +0500

    Оптимизция программы для удобства пользования

--- parser.py
+++ parser.py
@@ -2,8 +2,10 @@ import os
 
 print(f'Дорогой пользователь, пожалуйста пиши все названия директорий папок и т.д без ошибое, т.к из-за этого могут быть ошибки.\n')
 
-what = input('Вы готовы начать работу? (да/нет)? ').lower()
-
+what = ''
+while what == '':
+    what = input('Вы готовы начать работу? (да/нет)? ').lower()
+ 
 def parsing_manager(target_list):
     for i in range(len(target_list)):
         if key_word in target_list[i]:
@@ -19,7 +21,9 @@ def finder(folder):
 def main():
     for i in range(amount_promt):
         if amount_promt > 0:
-            folder_directory = input('Введите название папки: ')
+            folder_directory = ''
+            while folder_directory == '':
+                folder_directory = input('Введите название папки: ')
             try:
                 list = finder(folder_directory)
             except:
@@ -34,11 +38,17 @@ def main():
     print(f'Результат работы:\n{result}')
 
 if __name__ == '__main__':
-    while what != 'off':
+    while what != 'нет':
         if what == 'да':
-            directory = input('Введите директорию для работы: ')
-            key_word = input('Введите ключевое слово, по которому программа будет искать: ')
-            amount_promt = int(input('Введите кол-во папок (0 если нет): '))
+            directory = ''
+            while directory == '':
+                directory = input('Введите директорию для работы: ')
+            key_word = ''
+            while key_word == '':
+                key_word = input('Введите ключевое слово, по которому программа будет искать: ')
+            amount_promt = ''
+            while amount_promt == '':
+                amount_promt = int(input('Введите кол-во папок (0 если нет): '))
 
             folder_list = []
             result = []
@@ -47,4 +57,6 @@ if __name__ == '__main__':
         else:
             raise SystemExit
 
-        what = input('Вы хотите продолжить (да/нет)? ').lower()
+        what = ''
+        while what == '':
+            what = input('Вы хотите продолжить (да/нет)? ').lower()
